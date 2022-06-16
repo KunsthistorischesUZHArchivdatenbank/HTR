d988f092ce19f27de18f1b46327e13be96b2b1fb
PaulineJac <p.jacsont@gmail.com>
2022-06-16 17:36:14 +0200

    Model_seg

--- Data/submission-script-SEG.sh
+++ Data/submission-script-SEG.sh
@@ -9,12 +9,13 @@ module load fosscuda/2020b Python/3.8.6
 source ~/Sandozenv/bin/activate
 pip install albumentations
 
-work_directory="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Models/Model_SEG_1/"
+work_directory="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Models/SEG_illustrations_pages_1/"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
 
-XML_FOLDER="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Data/ManuscriptLines"
+XML_FOLDER="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Data/illustrations_pages"
 echo "KETOS training"
 srun ketos segtrain -f alto -d cuda "${XML_FOLDER}/*.xml"
+
--- Data/submission_script_HTR.sh
+++ Data/submission_script_HTR.sh
@@ -9,12 +9,12 @@ module load fosscuda/2020b Python/3.8.6
 source ~/Topenv/bin/activate
 pip install albumentations
 
-work_directory="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Models/ManuscriptLines/Model_HTR_2/"
+work_directory="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Model/HTR_PrintLines_1"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Data/ManuscriptLines"
+XML_FOLDER="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Data/PrintLines"
 
 echo "KETOS training"
 srun ketos train -o $OUTPUT_NAME -f alto -d cuda "${XML_FOLDER}/*.xml"
