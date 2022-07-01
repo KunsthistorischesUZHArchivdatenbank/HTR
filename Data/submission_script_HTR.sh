2168a8e6baf5ec6f5bd9fca1848fe37745361c36
PaulineJac <p.jacsont@gmail.com>
2022-07-01 17:03:34 +0200

    Models_2

--- Data/submission_script_HTR.sh
+++ Data/submission_script_HTR.sh
@@ -9,12 +9,12 @@ module load fosscuda/2020b Python/3.8.6
 source ~/Topenv/bin/activate
 pip install albumentations
 
-work_directory="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Models/HTR_PrintLines_1"
+work_directory="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Models/HTR_ManuscriptLines_2"
 mkdir -p ${work_directory}
 cd ${work_directory}
 
 OUTPUT_NAME="output_name"
-XML_FOLDER="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Data/PrintLines"
+XML_FOLDER="/home/users/j/jacsont/Kunsthistorisches-UZH_Archivdatenbank/HTR/Data/ManuscriptLines/"
 
 echo "KETOS training"
 srun ketos train -o $OUTPUT_NAME -f alto -d cuda "${XML_FOLDER}/*.xml"
